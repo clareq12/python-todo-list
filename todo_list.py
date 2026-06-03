4e45ce123c1394f24a79f595ce448a2231e1167e
clareq12 <ywtq7wn62f@privaterelay.appleid.com>
2026-06-03 14:17:52 +0100

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,27 @@
+tasks = []
+
+print("Python To-Do List")
+
+while True:
+    print("\n1. Add task")
+    print("2. View tasks")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print("Task added.")
+
+    elif choice == "2":
+        print("\nYour Tasks:")
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option.")
